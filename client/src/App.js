dfe02f373a5780337d487e6fef531b129347a679
GarrettF <grfoster24@gmail.com>
2021-09-11 09:05:03 -0400

    set up routes to screens from app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,30 @@
 import "./App.css"
+import { useState } from "react"
+import { Route, Switch } from "react-router-dom"
+import Home from "./screens/Home/Home"
+import About from "./screens/About/About"
+import JamDetail from "./screens/JamDetail/JamDetail"
+import JamForm from "./screens/JamForm/JamForm"
+import Jams from "./screens/Jams/Jams"
+import SignIn from "./screens/SignIn/SignIn"
+import SignUp from "./screens/SignUp/SignUp"
 
 function App() {
-  return <h1>This is the app comp. </h1>
+  // const [user, setUser] = useState()
+
+  return (
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/jams" component={Jams} />
+      <Route exact path="/jams/:id" component={JamDetail} />
+      <Route exact path="/jams/:id/edit" component={JamForm} />
+      {/* "/jams/new" routing to JamDetail instead of JamForm */}
+      <Route exact path="/jams/new" component={JamForm} />
+      <Route path="/signup" component={SignUp} />
+      <Route path="/signin" component={SignIn} />
+      <Route path="/about" component={About} />
+    </Switch>
+  )
 }
 
 export default App
