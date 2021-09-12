dc3905be2ed02b77d4e18ba9b3554b6aeb482a02
okamiffs <joshuaramnanan01@gmail.com>
2021-09-12 16:51:37 -0400

    Full auth backend

--- routes/jams.js
+++ routes/jams.js
@@ -1,12 +1,13 @@
 import { Router } from "express"
 import * as controllers from "../controllers/jams.js"
+import restrict from "../helpers/restrict.js"
 
 const router = Router()
 
 router.get("/jams", controllers.getJams)
 router.get("/jams/:id", controllers.getJam)
-router.post("/jams", controllers.createJam)
-router.put("/jams/:id", controllers.updateJam)
-router.delete("/jams/:id", controllers.deleteJam)
+router.post("/jams", restrict, controllers.createJam)
+router.put("/jams/:id", restrict, controllers.updateJam)
+router.delete("/jams/:id", restrict, controllers.deleteJam)
 
 export default router
--- routes/users.js
+++ routes/users.js
@@ -4,5 +4,7 @@ import * as controllers from '../controllers/users.js'
 const router = Router()
 
 router.post("/signup", controllers.signUp)
+router.post("/signin", controllers.signIn)
+router.get("/verify", controllers.verify)
 
 export default router
