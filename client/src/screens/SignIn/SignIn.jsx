916684be6ef761f37847c08a6cc6ba38fddfc78c
okamiffs <joshuaramnanan01@gmail.com>
2021-09-13 09:38:49 -0400

    Created form and tested to see if state works. (Josh + Casey)

--- client/src/screens/SignIn/SignIn.jsx
+++ client/src/screens/SignIn/SignIn.jsx
@@ -1,9 +1,27 @@
+import { useState } from "react"
 import Layout from "../../components/Layout/Layout"
 
 export default function SignIn() {
+  const [form, setForm] = useState({
+    email: "",
+    password: ""
+  })
+
+  const handleChange = (e) => {
+    const {name, value} = e.target
+    setForm({
+      ...form,
+      [name]: value
+    })
+  }
+
   return (
     <Layout>
-      <h1>SignIn Screen</h1>
+      <form>
+        <input type="text" name="email" id="email" placeholder="email" value={form.email} onChange={handleChange} />
+        <input type="password" name="password" id="password" placeholder="password" value={form.password} onChange={handleChange} />
+        <button type="submit">Sign in</button>
+      </form>
     </Layout>
   )
 }
