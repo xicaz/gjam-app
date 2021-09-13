4d9dc92fe676ec484d1ca90274480729901f6f18
Jexica Ayran <jexica@jexicas-air.myfiosgateway.com>
2021-09-13 16:39:07 -0400

    modal and more details link

--- client/src/components/JamCard/JamCard.jsx
+++ client/src/components/JamCard/JamCard.jsx
@@ -1,33 +1,19 @@
-import "./JamCard.css"
-import { Link } from "react-router-dom"
+import "./JamCard.css";
+import { Link } from "react-router-dom";
+import JamModal from "../JamModal/JamModal";
 
 export default function JamCard(props) {
-  const { name, price, _id } = props.jam
+  const { name, price, _id } = props.jam;
   return (
     <>
       <div className="jam-card">
+        <JamModal jam={props.jam} />
         <Link to={`/jams/${_id}`}>
           <img className="jam-card-image" src={props.imgURL} alt={name} />
         </Link>
         <h3>{name}</h3>
         <p>{price}</p>
       </div>
-
-      {/* inactive */}
-
-      {/* <div id="myModal" className="modal">
-        <div className="modal-content">
-          <span className="close">&times;</span>
-          <img className="jam-card-image" src={props.imgURL} alt={props.name} />
-          <h3>{props.name}</h3>
-          <p>{props.ingredients}</p>
-          <p>${props.price}</p>
-          <Link>View More</Link>
-        </div> */}
-
-      {/* </div> */}
     </>
-  )
+  );
 }
-
-// https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_modal
