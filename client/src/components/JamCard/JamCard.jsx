33189163bd1fa6aaead0732c8aeda5d7e75510c4
Jexica Ayran <jexica@jexicas-air.myfiosgateway.com>
2021-09-13 09:53:33 -0400

    Jam Card

--- client/src/components/JamCard/JamCard.jsx
+++ client/src/components/JamCard/JamCard.jsx
@@ -1,3 +1,42 @@
+import "./JamCard.css";
+import { Link } from "react-router-dom";
+
 export default function JamCard(props) {
-  return <h1>JamCard Component</h1>
+  return (
+    <>
+      <div className="jam-card">
+        <Link to={`/products/${props._id}`}>
+          <img className="jam-card-image" src={props.imgURL} alt={props.name} />
+        </Link>
+        <h3>{props.name}</h3>
+        <p>${props.price}</p>
+      </div>
+
+      {/* inactive */}
+
+      {/* <div id="myModal" className="modal">
+        <div className="modal-content">
+          <span className="close">&times;</span>
+          <img className="jam-card-image" src={props.imgURL} alt={props.name} />
+          <h3>{props.name}</h3>
+          <p>{props.ingredients}</p>
+          <p>${props.price}</p>
+          <Link>View More</Link>
+        </div> */}
+
+      {/* </div> */}
+    </>
+  );
 }
+
+// name: { type: String, required: true },
+// sweetness: { type: String, required: true },
+// spiciness: { type: String, required: true },
+// imgURL: { type: String, required: true },
+// description: { type: String, required: true },
+// creator: { type: String, required: true },
+// price: { type: String, required: true },
+// ingredients: [{ type: String, required: true }],
+// restricted: { type: Boolean, required: true },
+
+// https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_modal
