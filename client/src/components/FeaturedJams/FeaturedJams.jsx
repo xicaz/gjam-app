929bdf4c0c5e5dfdf66362d8064b4d3e8017127b
Jexica Ayran <jexica@jexicas-air.myfiosgateway.com>
2021-09-14 16:42:58 -0400

    added featured jams

--- client/src/components/FeaturedJams/FeaturedJams.jsx
+++ client/src/components/FeaturedJams/FeaturedJams.jsx
@@ -1,3 +1,30 @@
+import { useState, useEffect } from "react";
+import { getFeaturedJams } from "../../services/jams";
+import JamCard from "../JamCard/JamCard";
+
 export default function FeaturedJams(props) {
-  return <h1>FeaturedJams Component</h1>
+  const [featuredJams, setFeaturedJams] = useState([]);
+
+  useEffect(() => {
+    const fetchFeaturedJams = async () => {
+      const featuredJams = await getFeaturedJams();
+      setFeaturedJams(featuredJams);
+    };
+    fetchFeaturedJams();
+  }, []);
+
+  return (
+    <div className="featured-jams-container">
+      {featuredJams.map((jam, index) => {
+        return (
+          <JamCard
+            //imgURL={jam.imgURL}
+            imgURL={"https://i.imgur.com/m2o9NB9.png"}
+            jam={jam}
+            key={index}
+          />
+        );
+      })}
+    </div>
+  );
 }
