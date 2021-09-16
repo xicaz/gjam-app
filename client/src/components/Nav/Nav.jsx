5452f539e99bdda1e66142422573f8a5e646aaa6
casey <caseymcclenathan@Caseys-MacBook-Pro.local>
2021-09-15 21:43:51 -0500

    added hamburger

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -6,25 +6,28 @@ import {Navbar, NavDropdown, Nav} from 'react-bootstrap';
 export default function NavMenu(props) {
   return (
     <Nav>
-      <Navbar className="justify-content-end">
-        <NavDropdown title="products" id="products-dropdown">
-          <NavDropdown.Item><NavLink className="nav-link" to="/">premium</NavLink></NavDropdown.Item>
-          <NavDropdown.Item><NavLink className="nav-link" to="/jams">jams list</NavLink></NavDropdown.Item>
-        </NavDropdown>
-        <NavLink className="nav-link" to="/jams/new">create</NavLink>
-        <NavLink className="nav-link" to="/about">about</NavLink>
-        <NavDropdown title="account" id="products-dropdown">
-          {props.user
-          ? <>
-              <NavDropdown.Item><NavLink className="nav-link" to="/">sign out</NavLink></NavDropdown.Item>
-              <NavDropdown.Item><NavLink className="nav-link" to="/jams">jams list</NavLink></NavDropdown.Item>
-            </>
-          : <>
-              <NavDropdown.Item><NavLink className="nav-link" to="/signin">sign in</NavLink></NavDropdown.Item>
-              <NavDropdown.Item><NavLink className="nav-link" to="/signup">sign up</NavLink></NavDropdown.Item>
-            </>
-          }
-        </NavDropdown>
+      <Navbar collapseOnSelect expand="sm" className="navbar-dark justify-content-center">
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
+          <NavDropdown title="products" id="products-dropdown">
+            <NavDropdown.Item><NavLink className="nav-link" to="/">premium</NavLink></NavDropdown.Item>
+            <NavDropdown.Item><NavLink className="nav-link" to="/jams">jams list</NavLink></NavDropdown.Item>
+          </NavDropdown>
+          <NavLink className="nav-link" to="/jams/new">create</NavLink>
+          <NavLink className="nav-link" to="/about">about</NavLink>
+          <NavDropdown title="account" id="products-dropdown">
+            {props.user
+            ? <>
+                <NavDropdown.Item><NavLink className="nav-link" to="/">sign out</NavLink></NavDropdown.Item>
+                <NavDropdown.Item><NavLink className="nav-link" to="/jams">jams list</NavLink></NavDropdown.Item>
+              </>
+            : <>
+                <NavDropdown.Item><NavLink className="nav-link" to="/signin">sign in</NavLink></NavDropdown.Item>
+                <NavDropdown.Item><NavLink className="nav-link" to="/signup">sign up</NavLink></NavDropdown.Item>
+              </>
+            }
+          </NavDropdown>
+        </Navbar.Collapse>
       </Navbar>
     </Nav>
   );
