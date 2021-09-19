36d618ffd822e40e06f625a2b272ad7c9018ff91
casey <caseymcclenathan@Caseys-MacBook-Pro.local>
2021-09-19 17:06:22 -0500

    working on subtotal

--- client/src/screens/Cart/Cart.jsx
+++ client/src/screens/Cart/Cart.jsx
@@ -1,32 +1,32 @@
-import { useEffect, useState } from "react"
-import Layout from "../../components/Layout/Layout"
-import { getCart, getUser } from "../../services/users"
-import CartJam from "../../components/CartJam/CartJam"
-import Button from "react-bootstrap/Button"
+import { useEffect, useState } from "react";
+import Layout from "../../components/Layout/Layout";
+import { getUser, clearCart } from "../../services/users";
+import CartJam from "../../components/CartJam/CartJam";
+import Button from "react-bootstrap/Button";
 import "./Cart.css"
-import { clearCart } from "../../services/users"
-import { useHistory } from "react-router"
+import { useHistory } from "react-router";
+import { getJam } from "../../services/jams";
 
 export default function Cart(props) {
   const [isLoaded, setIsLoaded] = useState(false)
-  const [cart, setCart] = useState([])
+  const [userObj, setUserObj] = useState([])
   const [toggleFetch, setToggleFetch] = useState(false)
 
   const history = useHistory()
 
   useEffect(() => {
-    const fetchCart = async () => {
-      const cart = await getCart(props.user.id)
-      setCart(cart)
+    const fetchUser = async () => {
+      const user = await getUser(props.user.id)
+      console.log("user", user)
+      setUserObj(user)
       setIsLoaded(true)
+      console.log("subtotal in frontend", user.subtotal)
     }
-    if (props.user) {
-      fetchCart()
-    }
+    fetchUser()
   }, [props.user, toggleFetch])
 
-  if (!isLoaded) {
-    return (
+  if(!isLoaded) {
+    return(
       <Layout>
         <h1>issue</h1>
       </Layout>
@@ -34,40 +34,28 @@ export default function Cart(props) {
   }
 
   const handleClear = async () => {
-    const clear = await clearCart(props.user.id)
+    const clear = await clearCart(props.user.id);
     if (clear) {
       alert("cart cleared")
       history.push("/")
     }
   }
 
-  return (
+  return(
     <Layout user={props.user}>
-      <p> Subtotal = {subtotal} </p>
+      {/* <p>Subtotal: {userObj.subtotal}</p> */}
       <div className="clear-btn-container">
-        <Button
-          variant="outline-danger"
-          id="clear-btn"
-          className="clear-cart"
-          onClick={handleClear}
-        >
+        <Button variant="outline-danger" id="clear-btn" className="clear-cart" onClick={handleClear}>
           Clear Cart
         </Button>
       </div>
       <div className="cart-jams">
-        {cart.length === 0 ? <h1>Cart is empty!</h1> : null}
+        {userObj.cart.length === 0 ? <h1>Cart is empty!</h1> : null}
         <div className="jams-container">
-          {cart.map((cartItem, index) => (
-            <>
-              <CartJam
-                jam={cartItem.jamId}
-                quantity={cartItem.quantity}
-                key={index}
-                user={props.user}
-                setToggleFetch={setToggleFetch}
-              />
-            </>
-          ))}
+          {userObj.cart.map((cartItem, index) => <>
+            <CartJam jam={cartItem.jamId} quantity={cartItem.quantity} key={index} user={props.user} setToggleFetch={setToggleFetch}/>
+          </>
+          )}
         </div>
       </div>
     </Layout>
