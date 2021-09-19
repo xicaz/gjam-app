7a27ba1ece8ee861ec7e91f3c0e2be4c4e6ba919
Jexica Ayran <jexica@jexicas-air.myfiosgateway.com>
2021-09-19 13:48:45 -0400

    marquee added to banner component

--- client/src/components/Banner/Banner.jsx
+++ client/src/components/Banner/Banner.jsx
@@ -3,7 +3,48 @@ import "./Banner.css";
 export default function Banner() {
   return (
     <div className="featured-jams-banner">
-      <img className="img-crop" src="https://i.imgur.com/gIQ40c6.png" alt="banner" />
+      <div class="marquee">
+        <div class="marquee__inner" aria-hidden="true">
+          <span>
+            * g'jam with us * your jam is our jam * jamming to the beat of your
+            yum * bottled organic and fresh
+          </span>
+          <span>
+            * g'jam with us * your jam is our jam * jamming to the beat of your
+            yum * bottled organic and fresh
+          </span>
+          <span>
+            * g'jam with us * your jam is our jam * jamming to the beat of your
+            yum * bottled organic and fresh
+          </span>
+          <span>
+            * g'jam with us * your jam is our jam * jamming to the beat of your
+            yum * bottled organic and fresh
+          </span>
+          <span>
+            * g'jam with us * your jam is our jam * jamming to the beat of your
+            yum * bottled organic and fresh
+          </span>
+          <span>
+            * g'jam with us * your jam is our jam * jamming to the beat of your
+            yum * bottled organic and fresh
+          </span>
+          <span>
+            * g'jam with us * your jam is our jam * jamming to the beat of your
+            yum * bottled organic and fresh
+          </span>
+          <span>
+            * g'jam with us * your jam is our jam * jamming to the beat of your
+            yum * bottled organic and fresh
+          </span>
+        </div>
+      </div>
+      <div className="hr"></div>
+      <img
+        className="img-crop"
+        src="https://i.imgur.com/gIQ40c6.png"
+        alt="banner"
+      />
     </div>
   );
 }
